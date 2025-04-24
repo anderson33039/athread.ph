18e901fa10b2045894e8ce2e4c65a464458eb074
Jaycee <anderson.j4907@gmail.com>
2025-04-25 00:43:36 +0800

    part2
    day 2 - april 25 2025

--- assets/config.js
+++ assets/config.js
@@ -2,3 +2,12 @@ window.addEventListener('load',() => {
     document.getElementById('loadingscr').style.display = 'none';
     window.location.href = 'mts/mainpage.html';
 });
+function thread(num) {
+    var thread_code = {
+        1 : '../mts/library/01.html',
+        2 : '../mts/library/02.html',
+        3 : '../mts/library/03.html',
+        4 : "../mts/library/04.html",
+    }
+    window.location.href = thread_code[num];
+}
